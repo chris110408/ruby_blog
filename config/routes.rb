b1ab07a06d77d6c8b94de36ce5dc11d3ef8cb0e7
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-14 22:08:50 -0700

    remove jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
 
 
+  resources :articles
   # get 'pages/home', to: 'pages#home'
   #
   # get 'pages/about', to: 'pages#about'
@@ -15,7 +16,6 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/migrate/20160815050348_create_articles.rb
+++ db/migrate/20160815050348_create_articles.rb
@@ -1,16 +1,10 @@
 class CreateArticles < ActiveRecord::Migration
-  def up
-    # drop_table :articles
-
+  def change
     create_table :articles do |t|
       t.string :title
       t.text :description
 
       t.timestamps null: false
     end
-  end
-
-  def down
-    drop_table :articles
   end
 end
