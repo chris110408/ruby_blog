28fb54cd19e9a4b3cfa3ac152f9d89a164a10040
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-17 00:56:18 -0700

    add password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
   validates :username, presence:true, uniqueness:{case_sensitive: false}, length:{:minimum => 3, :maximum => 25}
   validates :email,  presence:true, uniqueness:{case_sensitive: false}, length:{:minimum => 3, :maximum => 105},
             :format =>EMAIL_REGEX
-
+   has_secure_password
 end
