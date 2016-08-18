42955d0e3098ebcfb3379b260bb314a376940c5b
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-17 22:20:46 -0700

    login and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'signup',to: 'users#new'
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
   resources :articles
   resources :users,except:[:new]
   # get 'pages/home', to: 'pages#home'
