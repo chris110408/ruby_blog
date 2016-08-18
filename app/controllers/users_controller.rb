1ac4d9e3e0321739f8b137fb41cf3e7eb28d4a18
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-17 23:55:08 -0700

    add admin delete function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_same_user,only:[:edit, :update]
+  before_action :require_same_user,only:[:edit, :update, :destroy]
+  before_action :require_admin, only:[:destroy]
+
   # GET /users
   def index
     @users = User.paginate(page: params[:page],per_page:5)
@@ -24,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "welcome to blog #{@user.username}"
       redirect_to articles_path
     else
@@ -44,7 +47,8 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    flash[:danger] = "User and all articles has been deleted"
+    redirect_to users_path
   end
 
   private
@@ -59,8 +63,15 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if !logged_in? || current_user != @user
+      if  current_user != @user && !current_user.admin?
         flash[:danger] = "you are only edit or delete your own account"
+        redirect_to root_path
+      end
+    end
+
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "only admin users can perform that action "
         redirect_to root_path
       end
     end
