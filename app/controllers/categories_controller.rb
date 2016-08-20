6389a11a9883d0c92cee4b8ffc4ce16ef883c00a
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-20 12:18:21 -0700

    cate/edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,8 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -18,6 +20,22 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category was update successfully"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page:params[:page],per_page:5)
