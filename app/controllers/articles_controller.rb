992263d6bcbffdc990d002e295af2156b00cc2f5
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-19 23:17:53 -0700

    revise article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description,category_ids:[])
     end
 
     def require_same_user
